3bd5b15b365c2fe30106dbb5aaf95e3ed5ef9f80
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2025-05-29 09:36:49 +0530

    Sum of Natural number

--- Maths/SumOfNaturalNumbers.py
+++ Maths/SumOfNaturalNumbers.py
@@ -1,3 +1,24 @@
 
+'''Naive Method'''
+
+def SumNat(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i
+    return sum
 
 
+res = SumNat(10)
+print(res)
+
+
+
+'''Efficient Method'''
+
+def SumNat1(n):
+    return (n*(n+1))//2
+
+
+res1 = SumNat1(10)
+print(res1)
+    
